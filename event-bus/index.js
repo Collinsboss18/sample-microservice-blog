da7dcb4651b365ee60026f609ba8e856f9275f8e
Innotexak <akuhinnocent2016@gmail.com>
2020-11-29 01:21:00 +0100

    Handling Moderation

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,6 +11,7 @@ app.post('/events', (req, res) => {
   const event = req.body;
 
   events.push(event);
+
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
   axios.post('http://localhost:4002/events', event);
