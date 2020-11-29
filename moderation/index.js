e842f1f6c808a9efd4937667ab29e4f6149cce84
Innotexak <akuhinnocent2016@gmail.com>
2020-11-29 01:20:14 +0100

    Creating Moderation

--- moderation/index.js
+++ moderation/index.js
@@ -10,15 +10,14 @@ app.post('/events', async (req, res) => {
 
   if (type === 'CommentCreated') {
     const status = data.content.includes('orange') ? 'rejected' : 'approved';
-
     await axios.post('http://localhost:4005/events', {
       type: 'CommentModerated',
       data: {
         id: data.id,
         postId: data.postId,
         status,
-        content: data.content
-      }
+        content: data.content,
+      },
     });
   }
 
